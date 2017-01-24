b19e26c028fbb789688374cf3b9c94e85ab53a3d
Kevin_Gu <kevin_gu@ehsy.com>
2017-01-24 23:47:09 +0800

    0124 update properties

--- user/src/main/java/com/gplucky/user/controller/UserController.java
+++ user/src/main/java/com/gplucky/user/controller/UserController.java
@@ -1,24 +1,28 @@
 package com.gplucky.user.controller;
 
 import com.gplucky.common.controller.BaseController;
+import com.gplucky.user.dao.UserMapper;
 import com.gplucky.user.model.User;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lenovo on 2017/1/22.
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController extends BaseController{
     private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    private UserMapper userMapper;
 
     @ApiOperation(value="获取用户信息", notes="根据id获取用户信息")
     @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
@@ -36,6 +40,6 @@ public class UserController extends BaseController{
     @RequestMapping(value="getInfo", method=RequestMethod.POST)
     public String getInfo(Long id, String name) {
         LOG.info("得到用户信息");
-        return "1111";
+        return userMapper.selectByPrimaryKey(1).getName();
     }
 }
