19e7cedebcf90c987a83008067c51a67b0a0e286
Kevin_Gu <kevin_gu@ehsy.com>
2017-02-07 17:28:54 +0800

    0207 initController 敏感接口，增加密码

--- task/src/main/java/com/gplucky/task/controller/InitController.java
+++ task/src/main/java/com/gplucky/task/controller/InitController.java
@@ -2,6 +2,8 @@ package com.gplucky.task.controller;
 
 import com.gplucky.task.service.StockRedisService;
 import com.gplucky.task.service.StockService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,48 +21,65 @@ public class InitController {
 
     private static final Logger LOG = LoggerFactory.getLogger(InitController.class);
 
+    private static final String PWD = "gbj";
+
     @Autowired
     private StockService stockService;
     @Autowired
     private StockRedisService stockRedisService;
 
-//    @ApiOperation(value="初始化股票连涨连跌", notes="初始化连涨连跌到redis中,切记！！先将股市最新数据及历史数据同步后执行！！")
+    @ApiOperation(value="初始化股票连涨连跌", notes="初始化连涨连跌到redis中,切记！！先将股市最新数据及历史数据同步后执行！！")
+    @ApiImplicitParam(name = "pwd", value = "敏感接口密码", required = true, dataType = "String")
     @RequestMapping(value="initStockUpAndDown", method = RequestMethod.POST)
     @ResponseBody
-    public String initStockUpAndDown(){
+    public String initStockUpAndDown(String pwd){
         LOG.info("初始化股票连涨连跌开始……");
-        boolean flg = stockRedisService.initStockSeqUpAndDown();
-        LOG.info("……结束初始化股票连涨连跌");
-        if(flg){
-            return "success";
+        if(PWD.equals(pwd)){
+            boolean flg = stockRedisService.initStockSeqUpAndDown();
+            LOG.info("……结束初始化股票连涨连跌");
+            if(flg){
+                return "success";
+            }else{
+                return "failed";
+            }
         }else{
             return "failed";
         }
     }
 
-//    @ApiOperation(value="初始化沪股列表", notes="初始化所有沪股信息")
+    @ApiOperation(value="初始化沪股列表", notes="初始化所有沪股信息")
+    @ApiImplicitParam(name = "pwd", value = "敏感接口密码", required = true, dataType = "String")
     @RequestMapping(value="initSHList", method = RequestMethod.POST)
     @ResponseBody
-    public String initSHList(){
+    public String initSHList(String pwd){
         LOG.info("初始化沪股列表开始……");
-        boolean flg = stockService.initSHList();
-        LOG.info("……结束初始化沪股列表");
-        if(flg){
-            return "success";
+        if(PWD.equals(pwd)){
+            boolean flg = stockService.initSHList();
+            LOG.info("……结束初始化沪股列表");
+            if(flg){
+                return "success";
+            }else{
+                return "failed";
+            }
         }else{
             return "failed";
         }
     }
 
-//    @ApiOperation(value="初始化深圳股市列表", notes="初始化所有深圳股市信息")
+    @ApiOperation(value="初始化深圳股市列表", notes="初始化所有深圳股市信息")
+    @ApiImplicitParam(name = "pwd", value = "敏感接口密码", required = true, dataType = "String")
     @RequestMapping(value="initSZList", method = RequestMethod.POST)
     @ResponseBody
-    public String initSZList(){
+    public String initSZList(String pwd){
         LOG.info("初始化深圳股市列表开始……");
-        boolean flg = stockService.initSZList();
-        LOG.info("……结束初始化深圳股市列表");
-        if(flg){
-            return "success";
+        if(PWD.equals(pwd)){
+            boolean flg = stockService.initSZList();
+            LOG.info("……结束初始化深圳股市列表");
+            if(flg){
+                return "success";
+            }else{
+                return "failed";
+            }
         }else{
             return "failed";
         }
