bfe96e4e75a0caf7c2cea8511190edbad77f6334
Kevin_Gu <kevin_gu@ehsy.com>
2017-02-07 21:51:59 +0800

    0207 新增ui模块  编写demo

---
+++ ui/src/main/java/com/gplucky/ui/Person.java
@@ -0,0 +1,34 @@
+package com.gplucky.ui;
+
+/**
+ * Created by ehsy_it on 2017/2/7.
+ */
+public class Person {
+    private String name;
+    private Integer age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
