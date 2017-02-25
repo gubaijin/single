4ebc977a8b53bfb9d027fd2eb20a4763070d25d4
Kevin_Gu <kevin_gu@ehsy.com>
2017-02-25 20:10:53 +0800

    0225  项目jenkins编译微调

--- monitor/src/main/java/com.gplucky.monitor/MonitorApplication.java
+++ monitor/src/main/java/com.gplucky.monitor/MonitorApplication.java
@@ -1,5 +1,6 @@
 package com.gplucky.monitor;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -13,4 +14,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableHystrixDashboard
 @EnableTurbine
 public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
 }
