86c3fc00e225ba91f51a82c1bfd4f8e6f760fa83
Kevin_Gu <kevin_gu@ehsy.com>
2017-02-26 20:27:24 +0800

    0226 task pom文件微调

--- common/src/main/java/com/gplucky/common/controller/HealthController.java
+++ common/src/main/java/com/gplucky/common/controller/HealthController.java
@@ -12,9 +12,18 @@ public class HealthController {
     @Value("${env}")
     private String env;
 
+    @Value("${juhe.stock.appkey}")
+    private String APP_KEY;
+
+    @Value("${juhe.url.stock.list.sh}")
+    private String STOCK_URL_LIST_SH;
+
+    @Value("${juhe.url.stock.list.sz}")
+    private String STOCK_URL_LIST_SZ;
+
     @RequestMapping("/health")
     public String health(){
-        return env;
+        return env + APP_KEY + STOCK_URL_LIST_SH + STOCK_URL_LIST_SZ;
     }
 
 }
