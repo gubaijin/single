65064a6c889a9f8d6a5115654b633f5f6b9b3595
Kevin_Gu <kevin_gu@ehsy.com>
2017-02-28 19:57:45 +0800

    0228  分页总页数bug

--- ui/src/main/java/com.gplucky.ui/controller/UpController.java
+++ ui/src/main/java/com.gplucky.ui/controller/UpController.java
@@ -34,7 +34,7 @@ public class UpController {
         int total = null == result.getPage()?0:result.getPage().getTotal();
         model.addAttribute("seqUpSetString", seqUpSet);
         model.addAttribute("pageNo", pageNo);
-        model.addAttribute("totalPage", total % Constants.PAGE_SIZE_10 == 0 ? (total / Constants.PAGE_SIZE_10) - 1 : total / Constants.PAGE_SIZE_10);
+        model.addAttribute("totalPage", total % Constants.PAGE_SIZE_10 == 0 ? total / Constants.PAGE_SIZE_10 : (total / Constants.PAGE_SIZE_10) + 1);
         model.addAttribute("PageCount", total);
         model.addAttribute("PageSize", Constants.PAGE_SIZE_10);
         model.addAttribute("countindex", pageNo * Constants.PAGE_SIZE_10);
